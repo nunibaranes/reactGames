a5b480194c63bd30382c617bc5955b8e29247b4d
nunibaranes <nunibaranes@gmail.com>
2019-03-20 21:32:23 +0200

    tested react-share

---
+++ src/app/components/common/share-buttons/ShareButtons.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  OKShareButton,
+  RedditShareButton,
+  TumblrShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  ViberShareButton,
+  WorkplaceShareButton,
+  LineShareButton,
+  EmailShareButton,
+
+
+  FacebookIcon,
+  TwitterIcon,
+  GooglePlusIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  VKIcon,
+  OKIcon,
+  TelegramIcon,
+  WhatsappIcon,
+  RedditIcon,
+  TumblrIcon,
+  MailruIcon,
+  EmailIcon,
+  LivejournalIcon,
+  ViberIcon,
+  WorkplaceIcon,
+  LineIcon,
+} from 'react-share';
+
+class SahreButtons extends Component {
+  constructor(props = {}) {
+    super(props);
+    this.state = {
+        shareOptions: [
+          {name:'FacebookShareButton', icon: 'FacebookIcon'},
+          {name:'LinkedinShareButton', icon: 'LinkedinIcon'},
+          {name:'TwitterShareButton', icon: 'TwitterIcon'},
+          {name:'WhatsappShareButton', icon: 'WhatsappIcon'},
+          {name:'PinterestShareButton', icon: 'PinterestIcon'},
+          {name:'MailruShareButton', icon: 'MailruIcon'},
+        ],
+        siteDetails: {
+          url: 'www.google.com',
+          title: 'Hello World',
+          description: 'Share buttons test'
+        }
+    };
+  }
+
+  render() {
+    const { shareOptions, siteDetails } = this.state;
+    return (
+      <section className="share-buttons wrapper wrap-with-border">
+        {
+          // shareOptions.map(option => {
+          //   const ShareTagName = option.name;
+          //   const IconTagName = option.icon;
+          //   console.log('ShareTagName ', ShareTagName)
+          //   console.log('IconTagName ', IconTagName)
+          //   return <ShareTagName url={siteDetails.url} quote={siteDetails.title}> 
+          //     <IconTagName
+          //       size={32}
+          //       round />
+          //   </ShareTagName>
+          // })
+          
+          <FacebookShareButton url={this.state.siteDetails.url} quote="it works">
+            <FacebookIcon
+                size={32}
+                round />
+          </FacebookShareButton>
+          
+        }
+      </section>
+    );
+  }
+}
+
+export default SahreButtons;
--- src/app/main/Main.js
+++ src/app/main/Main.js
@@ -3,12 +3,14 @@ import './Main.scss';
 
 // import ColorPicker from '../components/color-picker/ColorPicker';
 import Sudoku from '../components/sudoku/Sudoku';
+import ShareButtons from '../components/common/share-buttons/ShareButtons';
 
 class Main extends Component {
   render() {
     return (
       <section className="main" id="main">
         <Sudoku></Sudoku>
+        <ShareButtons></ShareButtons>
       </section>
     );
   }
