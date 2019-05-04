aa360a90aabbe3b8aba73e3526f0b95fe18d5710
Nofar Baranes <nofar@reallygood.co.il>
2019-05-04 21:42:46 +0300

    Added styles to conrollers buttons

--- src/app/components/game-of-life/controllers/Controllers.js
+++ src/app/components/game-of-life/controllers/Controllers.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
+import './Controllers.scss'
 import Title from '../../common/title/Title.js';
 class Controllers extends Component {
    static propTypes = {
@@ -37,33 +38,38 @@ class Controllers extends Component {
         const {title, gameIsRunning} = this.props;
 
         return (
-            <div className={this.getClasses()}>
-            <Title title={ title }></Title>
-                { 
-                // clearBoard button
-                <button 
-                    className='control-clear-board-game' 
-                    onClick={() => { this.props.onClickClearBoard() }}
-                >
-                    Clear
-                </button>
-                }
-                { // stopGame or runGame button
-                gameIsRunning ?
-                <button className="control-stop-game"
-                    onClick={() => { this.props.onClickStopGame() }}>Stop</button> :
-                <button className="control-run-game"
-                    onClick={() => { this.props.onClickRunGame() }}>Run</button>
-                }
-                { // setNextGenerationBoardStatus button
-                <button 
-                    className='control-next-generation' 
-                    onClick={() => { this.props.onClickSetNGBoardStaus() }}
-                >
-                    Next Generation
-                </button>
-                }
-            </div>
+            <section className={this.getClasses()}>
+                <Title title={ title }></Title>
+                <div>
+                    { // onClickClearBoard button
+                        <div 
+                            className='btn control-clear-board-game' 
+                            onClick={() => { this.props.onClickClearBoard() }}
+                        >
+                            Clear
+                        </div>
+                    }
+                    { // onClickStopGame or onClickRunGame button
+                        gameIsRunning ?
+                        <div className="btn control-stop-game"
+                            onClick={() => { this.props.onClickStopGame() }}>
+                            Stop
+                        </div> :
+                        <div className="btn control-run-game"
+                            onClick={() => { this.props.onClickRunGame() }}>
+                            Run
+                        </div>
+                    }
+                    { // onClickSetNGBoardStaus button
+                        <div 
+                            className='btn control-next-generation' 
+                            onClick={() => { this.props.onClickSetNGBoardStaus() }}
+                        >
+                            Next Generation
+                        </div>
+                    }
+                </div>
+            </section>
         );
     }
 }
