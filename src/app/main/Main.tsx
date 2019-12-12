ff9441286cb5f3dc8ae5705d8d721696fe24b849
Nofar Baranes <nofar@reallygood.co.il>
2019-12-12 15:31:23 +0200

    Removed colorPicker component

--- src/app/main/Main.tsx
+++ src/app/main/Main.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './Main.scss';
 
-// import ColorPicker from '../components/color-picker/ColorPicker';
 import Sudoku from '../components/sudoku/Sudoku';
 import GameOfLife from '../components/game-of-life/GameOfLife';
 
